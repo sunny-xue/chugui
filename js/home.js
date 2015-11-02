a87ecea60d5f072028d0f667602af6998161ba08
xsysunny <xsysunny@163.com>
2015-11-02 23:00:36 +0800

    增加首页二级导航以及图片特效

--- js/home.js
+++ js/home.js
@@ -1,2 +1,12 @@
-SlideShow(2000,"#slideContainer","#slidesImgs","#slideBar");
-SlideShow(2000,"#sliderSmallContainer","#slidesSmallImgs","#slideSmallBar");
+SlideShow(2000, "#slideContainer", "#slidesImgs", "#slideBar");
+SlideShow(2000, "#sliderSmallContainer", "#slidesSmallImgs", "#slideSmallBar");
+$(function() {
+    var productTypeList = $("#productTypeList li");
+    productTypeList.hover(
+        function() {
+            $(this).children("div.menu").show();
+        },
+        function() {
+            $(this).children("div.menu").hide();
+        });
+});
